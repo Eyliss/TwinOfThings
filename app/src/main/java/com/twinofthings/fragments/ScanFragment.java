7d23260ac2fce7b6c9fb2823ee9f34393c34f92c
Elizabeth Navarro Salva <elizabeth.navarro.1612@gmail.com>
2017-07-04 11:24:37 +0200

    Radar animation implementation

--- app/src/main/java/com/twinofthings/fragments/ScanFragment.java
+++ app/src/main/java/com/twinofthings/fragments/ScanFragment.java
@@ -5,6 +5,7 @@ import com.twinofthings.activities.ReaderActivity;
 
 import android.content.Context;
 import android.graphics.PorterDuff;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,15 +14,17 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class ScanFragment extends Fragment {
 
     private ImageButton mCloseButton;
-    private ImageView mScanIcon;
+    private RelativeLayout mImagesContainer;
     private TextView mScanTitle;
     private TextView mScanDescription;
     private ProgressBar mProgressBar;
+    AnimationDrawable rocketAnimation;
 
     public ScanFragment() {
 
@@ -50,7 +53,7 @@ public class ScanFragment extends Fragment {
 
     private void bindViews(View rootView){
         mCloseButton = (ImageButton)rootView.findViewById(R.id.close_button);
-        mScanIcon = (ImageView) rootView.findViewById(R.id.scanning_icon);
+        mImagesContainer = (RelativeLayout) rootView.findViewById(R.id.images_container);
         mScanTitle = (TextView) rootView.findViewById(R.id.scanning_title);
         mScanDescription = (TextView) rootView.findViewById(R.id.scanning_description);
         mProgressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
@@ -63,11 +66,21 @@ public class ScanFragment extends Fragment {
         });
 
         ((ReaderActivity) getActivity()).setActionBarTitle(R.string.scan_activity_title);
+
+        final ImageView rocketImage = (ImageView) rootView.findViewById(R.id.scan_clip);
+        rocketImage.setBackgroundResource(R.drawable.radar_animation);
+        rocketImage.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                rocketAnimation = (AnimationDrawable) rocketImage.getBackground();
+                                rocketAnimation.start();
+                            }
+                        });
     }
 
     //Configure UI for scanning
     public void startScan(){
-        mScanIcon.setImageResource(R.drawable.ic_scan_success);
+//        mScanIcon.setImageResource(R.drawable.ic_scan_success);
         mScanTitle.setText(R.string.scanning_successful);
         mScanTitle.setTextColor(getResources().getColor(android.R.color.holo_blue_bright));
         mScanDescription.setText(R.string.loading);
@@ -77,7 +90,7 @@ public class ScanFragment extends Fragment {
 
     //Configure UI when scan stops
     public void stopScan(){
-        mScanIcon.setImageResource(R.drawable.ic_scan);
+//        mScanIcon.setImageResource(R.drawable.ic_scan);
         mScanTitle.setText(R.string.scanning_tag_title);
         mScanTitle.setTextColor(getResources().getColor(android.R.color.white));
         mScanDescription.setText(R.string.scanning_tag_description);
